49ec4a3cdb9ffd29fa45f824d2a9be5ab26f6d1b
thanhthu2411 <thu24@byui.edu>
2025-07-05 19:28:27 -0600

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -3,10 +3,6 @@ const accordions = document.querySelectorAll(".accordion");
 accordions.forEach((accordion) => {
     accordion.addEventListener("click", function (){
         const panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+        panel.classList.toggle("show");
     })
 })
